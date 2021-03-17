c5c9b578bc6c8beb5b38c06e29b1d8b414078020
Ismael Baena <ismael.baena.7e3@itb.cat>
2021-03-17 12:49:55 +0100

    Update MovimientoFPS
    Se ha adaptado todo el movimiento a un parton de diseño state machine.
    
    Se ha añadido un doble salto.

---
+++ Assets/Scripts/Player/FPSMechanics/ControllerFPS.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerFPS : MonoBehaviour {
+
+    public enum State { walking, running, jumping, crounching, wallRunning, doubleJumping}
+    public State currentState;
+
+    public LayerMask suelo;
+    float timeNotCheckingGround;
+
+    CameraMovement camera;
+    PlayerMovementFPS movemenet;
+                            
+    void Start() {
+        movemenet = new PlayerMovementFPS(this.gameObject, suelo);
+    }
+
+    private void FixedUpdate() {
+        movemenet.Move();
+    }
+
+    void Update() {
+        CheckState();
+    }
+
+    private void CheckState() {
+        switch (currentState) {
+            case State.walking:
+                Jump();
+                Run();
+                Crounch();
+                break;
+            case State.running:
+                Jump();
+                StopRun();
+                Slide();
+                break;
+            case State.jumping:
+                Jump();
+                CheckGround();
+                break;
+            case State.doubleJumping:
+                CheckGround();
+                break;
+            case State.crounching:
+                Jump();
+                Run();
+                StopCrounch();
+                break;
+        }
+    }
+
+    private void Jump() {
+        if (InputsFPS.Jump()) {
+            movemenet.Jump();
+            if (currentState == State.jumping) { currentState = State.doubleJumping; }
+            else {
+                if (currentState == State.crounching) { movemenet.StopCrounch(); }
+                timeNotCheckingGround = Time.time + 1;
+                currentState = State.jumping;
+            }
+        }
+    }
+
+    private void CheckGround() {
+        if (movemenet.GroundCheck() && timeNotCheckingGround <= Time.time) { currentState = State.walking; }
+    }
+
+    private void Run() {
+        if (InputsFPS.Run()) {
+            if (currentState == State.crounching) { movemenet.StopCrounch(); }
+            movemenet.Sprint();
+            currentState = State.running;
+        }
+    }
+
+    private void Slide() {
+        if (InputsFPS.Crounch()) {
+            movemenet.Slide();
+            currentState = State.crounching;
+        }
+    }
+
+    private void StopRun() {
+        if (InputsFPS.StopRun()) {
+            movemenet.StopSprint();
+            currentState = State.walking;
+        }
+    }
+
+    private void Crounch() {
+        if (InputsFPS.Crounch()) {
+            movemenet.Crounch();
+            currentState = State.crounching;
+        }
+    }
+
+    private void StopCrounch() {
+        if (InputsFPS.Crounch()) {
+            movemenet.StopCrounch();
+            currentState = State.walking;
+        }
+    }
+ 
+
+    
+}
---
+++ Assets/Scripts/Player/FPSMechanics/PlayerMovementFPS.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementFPS {
+
+    public GameObject player;
+    public Rigidbody rb;
+
+    private Vector3 playerScale;
+    private Vector3 crouchScale = new Vector3(1, 0.5f, 1);
+
+    public float speed = 10f;
+    public float slideForce = 400f;
+    public float jumpForce = 10f;
+
+    Transform groundCheck;
+    public LayerMask groundMask;
+    public float groundDistance = 0.4f;
+
+    public PlayerMovementFPS(GameObject currentPlayer, LayerMask suelo) {
+        player = currentPlayer;
+        rb = currentPlayer.GetComponent<Rigidbody>();
+        groundCheck = currentPlayer.transform.GetChild(0).transform;
+        groundMask = suelo;
+        playerScale = player.transform.localScale;
+    }
+
+    public void Move() {
+        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        player.transform.Translate(x, 0, z);
+    }
+
+    public void Sprint() {
+        speed = speed * 2;
+    }
+
+    public void StopSprint() {
+        speed = speed / 2;
+    }
+
+    public void Slide()
+    {
+        Crounch();
+        rb.AddForce(player.transform.forward * slideForce);
+    }
+
+    public void Crounch() {
+        speed = speed / 2;
+        player.transform.localScale = crouchScale;
+        player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - 0.5f, player.transform.position.z);
+    }
+
+    public void StopCrounch() {
+        speed = speed * 2;
+        player.transform.localScale = playerScale;
+        player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 0.5f, player.transform.position.z);
+    }
+
+    public void Jump() {
+        rb.AddForce(player.transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    public bool GroundCheck() {
+        return Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+    }
+}
