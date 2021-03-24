5ffb7c2403e867d30fd336701539e90d791194dd
pauITB <73183443+pauITB@users.noreply.github.com>
2021-03-24 18:10:33 +0100

    Creando Eventos

---
+++ Assets/Scripts/Events/Evento.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Evento
+{
+    private float hora;
+    private void ejecutar()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player/3dPersonMechanics/EventsCollector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventsCollector : MonoBehaviour
+{
+    int day;
+    Evento[] eventos = new Evento[20];
+
+    public void setDay(int day)
+    {
+        this.day = day;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Evento[] eventosDia;
+        if (day == 1)
+        {
+            float numEvents = Random.Range(3, 6);
+            eventosDia = new Evento[(int)numEvents];
+            for(int i=0; i < eventosDia.Length; i++)
+            {
+                eventosDia.[i] = eventos[2]; //Random
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
