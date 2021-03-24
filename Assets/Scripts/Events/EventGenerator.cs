df08657579b874755e536adc97e956c18d57d794
pauITB <73183443+pauITB@users.noreply.github.com>
2021-03-24 23:46:58 +0100

    Empezando a gestionar eventos

---
+++ Assets/Scripts/Events/EventGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventGenerator : MonoBehaviour
+{
+    public GameObject[] eventosPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Instantiate(eventosPrefabs[0], Vector3.forward , Quaternion.identity);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
