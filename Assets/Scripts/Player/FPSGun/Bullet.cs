06e169208e82285001bc6f2df0478e1d51c218fd
IsmaelBaena <ismael.baena.7e3@itb.cat>
2021-04-07 16:26:14 +0200

    Update FPS gancho
    -GameObject que representa el arma creado.
    -Creación de las clases y herencias para el arma.
    -Creación del controlador del arma.
    -Script de gancho creado (falta testing)

---
+++ Assets/Scripts/Player/FPSGun/Bullet.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Bullet : MonoBehaviour {
+    public float dmg, maxDistance;
+
+    abstract public void BulletBehaivour();
+}
---
+++ Assets/Scripts/Player/FPSGun/GunController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+    Bullet bullet;
+    Shoot shoot;
+    Gancho gancho;
+
+    private bool isGrappling;
+
+    void Start() {  
+
+    
+    }
+
+    void Update() {
+        //shoot.ShootAction();
+    }
+}
