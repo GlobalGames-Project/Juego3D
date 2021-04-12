e774f4f4b242ff9d083800572766a1d0d1c87874
EloiAlsina <eloi.alsina.7e2@itb.cat>
2021-04-12 18:17:22 +0200

    Menu PopUp i camera controll
    Menú del PopUp (WIP) i s'esta implementant el controll de les cameras per l'habitació

--- Assets/Scripts/Events/PcController.cs
+++ Assets/Scripts/Events/PcController.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PcController : MonoBehaviour
 {
     public int id; // id para los dialogos
-                 
+    public GameObject popUpBox;
+    public Animator animator;
+    public TMP_Text popUpText;
+
+    public void PopUp(string text)
+    {
+        popUpBox.SetActive(true);
+        popUpText.text = text;
+        animator.SetTrigger("pop");
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
