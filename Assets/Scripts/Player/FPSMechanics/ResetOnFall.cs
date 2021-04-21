e4932f625fd1bf3b901ede05289c44ac8971d323
eriiiicbm <eric.bujaldon.7e2@itb.cat>
2021-04-21 13:26:44 +0200

    Reset al caer, también pierdes una vida

---
+++ Assets/Scripts/Player/FPSMechanics/ResetOnFall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetOnFall : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Check to see if the tag on the collider is equal to Enemy
+        if (other.tag == "FallHitbox")
+        {
+            GameManagerScript.health--;
+            transform.position = new Vector3(82, 69, 38);
+        }
+    }
+}
