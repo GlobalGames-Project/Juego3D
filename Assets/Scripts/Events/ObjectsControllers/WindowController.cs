1243d1e5462b2a27950083dda42997f6b48389bb
pauITB <73183443+pauITB@users.noreply.github.com>
2021-04-21 17:35:36 +0200

    MueblesPlantaInferior

--- Assets/Scripts/Events/ObjectsControllers/WindowController.cs
+++ Assets/Scripts/Events/ObjectsControllers/WindowController.cs
@@ -6,7 +6,7 @@ public class WindowController : MonoBehaviour
 {
 
      int idDialogo = (int)EnumDialogosId.dialogoAbrirVentana ; // id para los dialogos
-    int idEvent = (int)EnumEventosId.eventoAbrirVentana ;
+    int idEvent = (int)NightmareEventosEnum.EventosEnum.eventoMasVida; // id para eventos
 
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
