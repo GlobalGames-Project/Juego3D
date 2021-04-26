1741da11b452bf8e8cf87631dbf407ef94a104a5
Ismael Baena <ismael.baena.7e3@itb.cat>
2021-04-26 23:15:03 +0200

    Update tutorial

---
+++ Assets/Scripts/TextVisibility.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextVisibility : MonoBehaviour
+{
+
+Camera cam;
+
+    void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 viewPos = cam.WorldToViewportPoint(transform.position); // Grab the distance from the camera
+        if (viewPos.z < 10F) // How far away is it?
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
